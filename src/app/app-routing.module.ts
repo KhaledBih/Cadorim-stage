f1f10d35c62465dd34b3f7102781e5aaa3ae07bf
mohmedmahmoud <bouhmed199666@gmail.com/boycott>
2024-05-07 09:55:05 +0000

    traduction & guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
+import { authGuard } from './guards/auth-guard.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'crud', component: EmployeeComponent },
+  { path: 'crud', component: EmployeeComponent, canActivate: [authGuard] },
   { path: 'edit/:id', component: EmployeeEditComponent },
   { path: 'add-employee', component: HomeComponent },
 ];
